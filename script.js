b238f39e7432e1ba2714be7af22137da07529de4
Dheeraj Thind <dheerajthind@gmail.com>
2023-08-22 16:31:24 -0400

    finshed product

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
-//local history
-
 var searchHistory = localStorage.getItem('serchHistory')
 
 
-//forcast api call `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=22ffcf9ab3ce5bcf313e65a60e0935fc`
 
 function searchCity(cityName){
+    //api url
     let apiurl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=22ffcf9ab3ce5bcf313e65a60e0935fc`    
     
     fetch(apiurl)
     .then((Response) => {
-      
-      
+      //turn response into json
       return Response.json();
     })
     .then((data) => {
-     
+      
+      //thow invlaid data if data does not exist
+      
+
+      if(data.length === 0){
+        //throw error
+        throw new Error('No city was found with that name')
+      }
+
+      //add button add save data
+      if(!searchHistory.includes(data[0].name)){
+        let buttonelemnt = $(`<div class="col-12 m-2"> <button type="button" class="btn btn-info btn-block">${data[0].name}</button> </div>`)
+        // $('<div class="col-12 m-2"> </div>')
+        buttonelemnt.attr('data-location', data[0].name)
+        buttonelemnt.on('click', (event) =>{
+            let location = event.target.textContent
+           
+            searchCity(location)
+        } )
+        $('#searchHistoryContainer').append(buttonelemnt)
+
+        searchHistory.push(data[0].name)
+
+        localStorage.setItem('serchHistory', JSON.stringify(searchHistory))
+      }
+
 
       return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=22ffcf9ab3ce5bcf313e65a60e0935fc&units=metric
       `)
+
+
     }).then((Response) => {
       return Response.json()
     }).then((data) => {
@@ -26,8 +50,7 @@ function searchCity(cityName){
       let todayCard = $('#todayForcast')
     
 
-      // let header = $(`${data.name} <span><img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt=""></span>`)
-//(data.name + " " + dayjs().format('M/D/YYYY'))
+  
       todayCard.children('h2').eq(0).children('p').text((data.name + " " + dayjs().format('M/D/YYYY')))
       todayCard.children('h2').eq(0).children('span').eq(0).children('img').eq(0).attr('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png` )
       //todayCard.children('h2').eq(0).children('span').eq(0).attr('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`)
@@ -60,17 +83,15 @@ function searchCity(cityName){
         fristCard.children('p').eq(3).text('Humidity: ' + data.list[index].main.humidity + " %")
       }
 
-      // fristCard.children('p').eq(0).text( data.list[0].dt_txt.split(' ')[0].replaceAll('-','/'))
-      // fristCard.children('img').eq(0).attr('src', `http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`)
-      // fristCard.children('p').eq(1).text("Temp: " +data.list[0].main.temp + ' °C')
-      // fristCard.children('p').eq(2).text('Wind: ' + data.list[0].wind.speed + ' KPH')
-      // fristCard.children('p').eq(3).text('Humidity: ' + data.list[0].main.humidity + " %")
+      
 
 
-
+    }).catch( (error) => {
+      window.alert('A city with that name was not found')
     });
 }
 
+//load the data when script is loaded
 if(searchHistory !== null){
     searchHistory = JSON.parse(searchHistory)
 
@@ -90,31 +111,20 @@ if(searchHistory !== null){
     searchHistory = []
 }
 
-
-//all event handlers 
+//Histroy Button Event Handler
 function buttonClickHandler(){
     let SearchBar = $('#SearchBar')
 
+    //stop code if search bar is empty
     if(!SearchBar.val()){
       return 
     }
     
-    let buttonelemnt = $(`<div class="col-12 m-2"> <button type="button" class="btn btn-info btn-block">${SearchBar.val()}</button> </div>`)
-        // $('<div class="col-12 m-2"> </div>')
-        buttonelemnt.attr('data-location', SearchBar.val())
-        buttonelemnt.on('click', (event) =>{
-            let location = event.target.textContent
-           
-            searchCity(location)
-        } )
-        $('#searchHistoryContainer').append(buttonelemnt)
+    
+    //search city if the serach has a value
+    searchCity(SearchBar.val().split(' ')[0])
 
-
-    searchCity(SearchBar.val())
-
-    searchHistory.push(SearchBar.val())
-
-    localStorage.setItem('serchHistory', JSON.stringify(searchHistory))
+    //clear the search bar
     SearchBar.val('')
     
 }
