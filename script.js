432eaef01f73897318a6a7796adfce23a6ac08e5
Dheeraj Thind <dheerajthind@gmail.com>
2023-08-21 20:15:02 -0400

    added current weather

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 var searchHistory = localStorage.getItem('serchHistory')
 
 
+//forcast api call `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=22ffcf9ab3ce5bcf313e65a60e0935fc`
+
 function searchCity(cityName){
     let apiurl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=22ffcf9ab3ce5bcf313e65a60e0935fc`    
     
@@ -15,11 +17,22 @@ function searchCity(cityName){
     .then((data) => {
       console.log(data);
 
-      return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=22ffcf9ab3ce5bcf313e65a60e0935fc`)
+      return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=22ffcf9ab3ce5bcf313e65a60e0935fc&units=metric
+      `)
     }).then((Response) => {
       return Response.json()
     }).then((data) => {
       console.log(data)
+      let todayCard = $('#todayForcast')
+
+      todayCard.children('h2').eq(0).text(data.name)
+      todayCard.children('p').eq(0).text('Temp: '+ data.main.temp)
+      todayCard.children('p').eq(1).text('Wind: '+ data.wind.speed)
+      todayCard.children('p').eq(2).text('Humidity: '+ data.main.humidity)
+
+      return fetch('https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=22ffcf9ab3ce5bcf313e65a60e0935fc&units=metric')
+    }).then(Response => {
+      return Response.json()
     });
 }
 
@@ -31,7 +44,8 @@ if(searchHistory !== null){
         // $('<div class="col-12 m-2"> </div>')
         buttonelemnt.attr('data-location', element)
         buttonelemnt.on('click', (event) =>{
-            let location = event.target.attr('data-location')
+            let location = event.target.textContent
+            console.log(location)
             searchCity(location)
         } )
         $('#searchHistoryContainer').append(buttonelemnt)
